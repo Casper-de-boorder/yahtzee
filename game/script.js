7a20c12465c2a5abba8853d89fc3eb8a796d8bc7
Casper-de-boorder <127783728+Casper-de-boorder@users.noreply.github.com>
2023-03-13 19:24:46 +0100

    Add files via upload

---
+++ game/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var rolls = 3,
+bonusTotal = 0,
+leftTotal = 0,
+rightTotal = 0,
+grandTotal = 0,
+yBonus = false,
+gameOver = false,
+roundComplete = 0,
+have5 = false;
+
+document.querySelector('.rol').addEventListener('click', function() {
+    console.log('hoi')
+});
+
